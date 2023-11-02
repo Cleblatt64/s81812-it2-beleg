27f805cd116375779e44bc8e1dd418728c79823b
Cleblatt64 <cleblatt.64@yahoo.com>
2023-11-02 10:09:53 +0100

    Core Classes generated

--- src/Rtsp.java
+++ src/Rtsp.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.logging.Level;
 
 public class Rtsp extends RtspDemo{
     public Rtsp(BufferedReader RTSPBufferedReader, BufferedWriter RTSPBufferedWriter, int rtpRcvPort, String rtspUrl, String videoFileName) {
@@ -12,32 +13,113 @@ public class Rtsp extends RtspDemo{
 
     @Override
     boolean play() {
-        return false;
+        if (state != State.READY) {
+            logger.log(Level.WARNING, "RTSP state: " + state);
+            return false;
+        }
+        RTSPSeqNb++;
+        send_RTSP_request("PLAY");
+        // Wait for the response
+        logger.log(Level.INFO, "Wait for response...");
+        if (parse_server_response() != 200) {
+            logger.log(Level.WARNING, "Invalid Server Response");
+            return false;
+        } else {
+            state = State.PLAYING;
+            logger.log(Level.INFO, "New RTSP state: " + state + "\n");
+            return true;
+        }
     }
 
     @Override
     boolean pause() {
-        return false;
+        if (state != State.PLAYING) {
+            logger.log(Level.WARNING, "RTSP state: " + state);
+            return false;
+        }
+        RTSPSeqNb++;
+        send_RTSP_request("PAUSE");
+        // Wait for the response
+        logger.log(Level.INFO, "Wait for response...");
+        if (parse_server_response() != 200) {
+            logger.log(Level.WARNING, "Invalid Server Response");
+            return false;
+        } else {
+            state = State.READY;
+            logger.log(Level.INFO, "New RTSP state: " + state + "\n");
+            return true;
+        }
     }
 
     @Override
     boolean teardown() {
-        return false;
+        if (state != State.PLAYING && state != State.READY) {
+            logger.log(Level.WARNING, "RTSP state: " + state);
+            return false;
+        }
+        RTSPSeqNb++;
+        send_RTSP_request("TEARDOWN");
+        // Wait for the response
+        logger.log(Level.INFO, "Wait for response...");
+        if (parse_server_response() != 200) {
+            logger.log(Level.WARNING, "Invalid Server Response");
+            return false;
+        } else {
+            state = State.INIT;
+            logger.log(Level.INFO, "New RTSP state: " + state + "\n");
+            return true;
+        }
     }
 
     @Override
     void describe() {
-
+        RTSPSeqNb++;
+        send_RTSP_request("DESCRIBE");
+        // Wait for the response
+        logger.log(Level.INFO, "Wait for response...");
+        if (parse_server_response() != 200) {
+            logger.log(Level.WARNING, "Invalid Server Response");
+        }
     }
 
     @Override
     void options() {
-
+        RTSPSeqNb++;
+        send_RTSP_request("OPTIONS");
+        // Wait for the response
+        logger.log(Level.INFO, "Wait for response...");
+        if (parse_server_response() != 200) {
+            logger.log(Level.WARNING, "Invalid Server Response");
+        }
     }
 
     @Override
     void send_RTSP_request(String request_type) {
-
+        try {
+            switch (request_type) {
+                case "OPTIONS" -> {
+                    RTSPBufferedWriter.write("OPTIONS * RTSP/1.0\n");
+                    RTSPBufferedWriter.write("CSeq: "+RTSPSeqNb+"\n\n");
+                }
+                case "DESCRIBE" -> {
+                    RTSPBufferedWriter.write("DESCRIBE "+rtspUrl+VideoFileName+" RTSP/1.0\n");
+                    RTSPBufferedWriter.write("CSeq: "+RTSPSeqNb+"\n\n");
+                }
+                case "SETUP" -> {
+                    RTSPBufferedWriter.write("SETUP "+rtspUrl+VideoFileName+"/trackID=0 RTSP/1.0\n");
+                    RTSPBufferedWriter.write("CSeq: "+RTSPSeqNb+"\n");
+                    RTSPBufferedWriter.write("Transport: RTP/AVP;unicast;client_port=25000-25001\n\n");
+                }
+                case "PLAY","PAUSE","TEARDOWN" -> {
+                    RTSPBufferedWriter.write(request_type+" " + rtspUrl + VideoFileName + " RTSP/1.0\n");
+                    RTSPBufferedWriter.write("CSeq: " + RTSPSeqNb + "\n");
+                    RTSPBufferedWriter.write("Session: 123456\n\n");
+                }
+            }
+            RTSPBufferedWriter.flush();
+        } catch (Exception e){
+            System.exit(-1);
+        };
     }
 
     @Override
