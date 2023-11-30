6b0431aad9fca17d6d16fa31154b969eb82638d5
Cleblatt64 <cleblatt.64@yahoo.com>
2023-11-30 10:17:48 +0100

    fixed options describe

--- src/FecHandler.java
+++ src/FecHandler.java
@@ -11,11 +11,22 @@ public class FecHandler extends FecHandlerDemo{
 
     @Override
     boolean checkCorrection(int nr, HashMap<Integer, RTPpacket> mediaPackets) {
-        return false;
+        if (fecList.get(fecNr.get(nr)) == null || fecList.get(nr) == null) return false;
+        int c=0;
+        for (int i : fecList.get(nr)){
+            if (mediaPackets.get(i) == null) c++;
+        }
+        return c<=1;
     }
 
     @Override
     RTPpacket correctRtp(int nr, HashMap<Integer, RTPpacket> mediaPackets) {
-        return null;
+
+        FECpacket p = fecStack.get(fecNr.get(nr));
+        for (int i : fecList.get(nr)){
+            if (i == nr) continue;
+            p.addRtp(mediaPackets.get(i));
+        }
+        return p.getLostRtp(nr);
     }
 }
